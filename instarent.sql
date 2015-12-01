eb8541f858fea4dec27364447f9eb5ede99a5e0b
Grey1 <arjun90.nair@gmail.com>
2015-12-01 22:30:02 +0530

    Signed-off-by: Grey1 <arjun90.nair@gmail.com>

--- instarent.sql
+++ instarent.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 04, 2015 at 01:19 PM
+-- Generation Time: Dec 01, 2015 at 05:59 PM
 -- Server version: 5.6.26
 -- PHP Version: 5.6.12
 
@@ -38,17 +38,16 @@ CREATE TABLE IF NOT EXISTS `amenities` (
   `elevator` varchar(5) NOT NULL,
   `parking` varchar(5) NOT NULL,
   `essentials` varchar(5) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `amenities`
 --
 
 INSERT INTO `amenities` (`amenity_id`, `venue_id`, `workspace_id`, `wifi`, `internet`, `kitchen`, `doorman`, `buzzer`, `elevator`, `parking`, `essentials`) VALUES
-(1, 1, 1, 'YES', 'YES', 'YES', 'YES', 'YES', 'YES', 'YES', 'YES'),
-(2, 2, 2, 'NO', 'NO', 'NO', 'NO', 'YES', 'YES', 'NO', 'NO'),
-(3, 3, 3, 'NO', 'NO', 'NO', 'NO', 'YES', 'YES', 'NO', 'NO'),
-(4, 4, 0, '', '', '', '', '', '', '', '');
+(1, 1, 1, '', 'YES', '', '', '', 'YES', 'YES', ''),
+(2, 2, 2, 'YES', 'YES', '', '', '', '', '', 'YES'),
+(3, 3, 3, '', 'YES', '', '', '', '', '', 'YES');
 
 -- --------------------------------------------------------
 
@@ -71,37 +70,15 @@ CREATE TABLE IF NOT EXISTS `booking` (
   `booking_status` int(11) NOT NULL,
   `team_size` int(11) NOT NULL,
   `rejected_by_host` tinyint(1) NOT NULL DEFAULT '0'
-) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `booking`
 --
 
 INSERT INTO `booking` (`booking_id`, `user_id`, `host_id`, `confirmation_from_host`, `from_time`, `to_time`, `from_date`, `to_date`, `payment_status`, `workspace_id`, `total_amount`, `booking_status`, `team_size`, `rejected_by_host`) VALUES
-(5, 1, 2, 0, '00:00:00', '00:00:00', '2015-11-15', '2015-11-22', 0, 1, 0, 0, 0, 0),
-(6, 1, 2, 0, '00:00:00', '00:00:00', '2015-11-04', '2015-11-08', 0, 1, 0, 0, 0, 0),
-(7, 1, 2, 0, '00:00:00', '00:00:00', '2014-11-04', '2014-11-05', 0, 1, 0, 0, 0, 0),
-(8, 1, 2, 0, '00:00:00', '00:00:00', '2014-11-04', '2014-11-05', 0, 2, 0, 0, 0, 0),
-(9, 1, 2, 0, '00:00:00', '00:00:00', '2014-11-04', '2014-11-05', 0, 2, 0, 0, 0, 0),
-(10, 1, 2, 0, '00:00:00', '00:00:00', '2014-11-04', '2014-11-05', 0, 2, 0, 0, 0, 0),
-(11, 0, 2, 0, '00:00:00', '00:00:00', '2015-11-04', '2015-12-09', 0, 3, 696.45161290323, 0, 0, 0),
-(12, 0, 2, 0, '00:00:00', '00:00:00', '2015-11-05', '2015-11-06', 0, 3, 160, 0, 0, 0),
-(13, 1, 2, 0, '00:00:00', '00:00:00', '2015-11-04', '2015-11-07', 0, 3, 480, 0, 0, 0),
-(14, 1, 2, 0, '00:00:00', '00:00:00', '2015-11-04', '2015-11-07', 0, 3, 480, 0, 10, 0),
-(15, 1, 2, 0, '00:00:00', '00:00:00', '2015-11-17', '2015-11-19', 0, 3, 320, 0, 50, 0),
-(16, 1, 2, 0, '00:00:00', '00:00:00', '2015-11-10', '2015-11-12', 0, 3, 320, 0, 50, 0),
-(17, 1, 2, 0, '00:00:00', '00:00:00', '2015-11-04', '2015-11-18', 0, 3, 40, 0, 50, 0),
-(18, 1, 2, 0, '00:00:00', '00:00:00', '2015-11-04', '2015-11-24', 0, 3, 177.14285714286, 0, 80, 0),
-(19, 1, 2, 0, '00:00:00', '00:00:00', '2015-11-04', '2015-11-24', 0, 3, 177.14285714286, 0, 50, 0),
-(20, 1, 2, 0, '00:00:00', '00:00:00', '2015-11-04', '2015-11-25', 0, 3, 60, 0, 50, 0),
-(21, 1, 2, 0, '00:00:00', '00:00:00', '2015-11-05', '2015-11-24', 0, 3, 154.28571428571, 0, 80, 0),
-(22, 1, 2, 0, '00:00:00', '00:00:00', '2015-11-04', '2015-11-18', 0, 3, 40, 0, 20, 0),
-(23, 1, 2, 0, '00:00:00', '00:00:00', '2015-11-10', '2015-11-12', 0, 3, 320, 0, 20, 0),
-(24, 1, 2, 0, '00:00:00', '00:00:00', '2015-11-04', '2015-11-11', 0, 3, 20, 0, 1, 0),
-(25, 1, 2, 0, '00:00:00', '00:00:00', '2015-11-04', '2015-11-12', 0, 3, 42.857142857143, 0, 5, 0),
-(26, 1, 2, 0, '00:00:00', '00:00:00', '2015-11-05', '2015-11-17', 0, 3, 134.28571428571, 0, 50, 0),
-(27, 1, 2, 0, '00:00:00', '00:00:00', '2015-11-04', '2015-11-11', 0, 3, 20, 0, 1, 0),
-(28, 1, 2, 0, '00:00:00', '00:00:00', '2015-11-11', '2015-11-12', 0, 3, 160, 0, 50, 0);
+(1, 3, 2, 1, '00:00:00', '00:00:00', '2015-11-01', '2015-11-03', 0, 1, 6000, 0, 1, 0),
+(2, 3, 4, 1, '00:00:00', '00:00:00', '2015-11-04', '2015-11-05', 0, 2, 5000, 0, 1, 0);
 
 -- --------------------------------------------------------
 
@@ -138,16 +115,15 @@ CREATE TABLE IF NOT EXISTS `reviews` (
   `author_id` int(20) NOT NULL,
   `host_id` int(20) NOT NULL,
   `createdOn` date NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `reviews`
 --
 
 INSERT INTO `reviews` (`review_id`, `booking_id`, `workspace_id`, `venue_id`, `stars`, `body`, `author_id`, `host_id`, `createdOn`) VALUES
-(1, 6, 3, 2, 2, 'test 123', 1, 2, '2015-11-20'),
-(2, 6, 3, 2, 4, 'test 123', 1, 2, '2015-11-19'),
-(3, 6, 2, 2, 4, 'test 123', 1, 2, '2015-11-01');
+(1, 1, 1, 1, 5, 'This was the best experience !!!', 3, 2, '2015-11-06'),
+(2, 2, 2, 2, 5, 'WOnderful Place', 3, 4, '2015-11-06');
 
 -- --------------------------------------------------------
 
@@ -192,15 +168,17 @@ CREATE TABLE IF NOT EXISTS `user` (
   `user_detail_id` int(50) NOT NULL,
   `usertype` varchar(15) NOT NULL,
   `verified` tinyint(1) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`userid`, `email`, `password`, `first_name`, `surname`, `active`, `user_detail_id`, `usertype`, `verified`) VALUES
-(1, 'arjun90.nair@gmail.com', 'arjun123', 'Arjun', 'Nair', 1, 1, 'user', 0),
-(2, 'arjun@nair.com', 'test12', 'Vighnesh', 'Nair', 1, 2, 'user', 0);
+(1, 'b@allen.com', 'barry12', 'barry', 'allen', 1, 1, 'admin', 0),
+(2, 'arjun@host.com', 'arjun123', 'arjun', 'nair', 1, 2, 'user', 0),
+(3, 'luke@gmail.com', 'luke123', 'Luke', 'Shaw', 1, 3, 'user', 0),
+(4, 'vig@gmail.com', '12345a', 'Vighnesh', 'Nair', 1, 4, 'user', 0);
 
 -- --------------------------------------------------------
 
@@ -219,16 +197,19 @@ CREATE TABLE IF NOT EXISTS `user_details` (
   `address2` varchar(100) NOT NULL,
   `pincode` int(6) NOT NULL,
   `city` varchar(50) NOT NULL,
-  `photo_path` varchar(100) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+  `photo_path` varchar(100) NOT NULL,
+  `about` varchar(500) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user_details`
 --
 
-INSERT INTO `user_details` (`user_detail_id`, `user_id`, `gender`, `birth_date`, `contact`, `company_name`, `address1`, `address2`, `pincode`, `city`, `photo_path`) VALUES
-(1, 1, 2, '2015-11-03', '9768019332', 'Greytrix', '', '', 0, '', 'images/detailsquare3.jpg'),
-(2, 2, 0, '0000-00-00', '', '', '', '', 0, '', 'images/detailsquare3.jpg');
+INSERT INTO `user_details` (`user_detail_id`, `user_id`, `gender`, `birth_date`, `contact`, `company_name`, `address1`, `address2`, `pincode`, `city`, `photo_path`, `about`) VALUES
+(1, 1, 0, '0000-00-00', '0', '', '', '', 0, '', '', ''),
+(2, 2, 2, '0000-00-00', '976801932', 'Greytrix', '', '', 0, '', 'images/avatar.jpg', 'Test'),
+(3, 3, 2, '0000-00-00', '656595959', 'Stark', '', '', 0, '', 'images/detailsquare3.jpg', 'Me awesome'),
+(4, 4, 2, '0000-00-00', '0', 'KFC', '', '', 0, '', 'images/basketsquare.jpg', 'Hi i am Vighnesh');
 
 -- --------------------------------------------------------
 
@@ -249,8 +230,8 @@ CREATE TABLE IF NOT EXISTS `venue` (
   `floor_area` int(11) NOT NULL,
   `no_of_rooms` int(11) NOT NULL,
   `no_of_desks` int(11) NOT NULL,
-  `opening_time` time NOT NULL,
-  `closing_time` time NOT NULL,
+  `opening_time` varchar(10) NOT NULL,
+  `closing_time` varchar(10) NOT NULL,
   `addr` longtext NOT NULL,
   `neighbourhood` varchar(50) NOT NULL,
   `telephone` varchar(20) NOT NULL,
@@ -259,29 +240,39 @@ CREATE TABLE IF NOT EXISTS `venue` (
   `workspace_id` int(20) NOT NULL,
   `verified` tinyint(1) NOT NULL,
   `available_days` int(2) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `venue`
 --
 
 INSERT INTO `venue` (`venue_id`, `user_id`, `name`, `event_type`, `city`, `state`, `venue_desc`, `logo_addr`, `no_of_floors`, `floor_area`, `no_of_rooms`, `no_of_desks`, `opening_time`, `closing_time`, `addr`, `neighbourhood`, `telephone`, `email`, `website`, `workspace_id`, `verified`, `available_days`) VALUES
-(1, '1', 'My first venue', '2', 'Jordan', '', 'test222', '', 12, 5, 1, 1, '00:00:00', '00:00:00', 'test123', 'test', '9', 't@t', '', 1, 0, 0),
-(2, '1', 'My first venue', '2', 'Jordan', '', 'test', '', 1, 1, 1, 1, '00:00:00', '00:00:00', 'test123', 'test', '9', 't@t', '', 2, 0, 0),
-(3, '2', 'My first venue', '2', 'Jordan', '', 'test', '', 1, 1, 1, 1, '00:00:00', '00:00:00', 'test123', 'test', '9', 't@t', '', 3, 0, 0);
+(1, '2', 'EV crystal', '1', 'Mumbai', '', 'Perfect office space at a Growing city', '', 7, 500, 2, 12, '', '', 'Koparkhairane', 'Thane', '02512352003', 'vig@gmail.com', '', 1, 0, 0),
+(2, '4', 'Lok Dhara', '1', 'Mumbai', '', 'This a Perfect Place For Start ups', '', 7, 522, 1, 10, '', '', 'kalyan', 'Thane', '2352003', 'mmj@gm.com', 'http://www.geekviewpoint.com/', 2, 0, 0),
+(3, '4', 'Lok Dhara', '1', 'Mumbai, Maharashtra, India', '', 'This a test', '', 6, 200, 2, 10, '', '', 'Thane', 'mumbai', '0213552', 's@gmail.com', '', 3, 0, 0);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `verification_photos`
+-- Table structure for table `verification`
 --
 
-CREATE TABLE IF NOT EXISTS `verification_photos` (
-  `verification_photos_id` int(20) NOT NULL,
+CREATE TABLE IF NOT EXISTS `verification` (
   `verification_id` int(20) NOT NULL,
-  `verification_name` varchar(50) NOT NULL,
-  `image_path` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `workspace_id` int(20) NOT NULL,
+  `img_path` varchar(100) NOT NULL,
+  `verify` int(10) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `verification`
+--
+
+INSERT INTO `verification` (`verification_id`, `workspace_id`, `img_path`, `verify`) VALUES
+(1, 1, '', 1),
+(3, 2, '', 1),
+(4, 2, '', 1),
+(5, 3, '', 1);
 
 -- --------------------------------------------------------
 
@@ -311,16 +302,16 @@ CREATE TABLE IF NOT EXISTS `workspace` (
   `listedbyuser` tinyint(1) NOT NULL,
   `verifiedbyadmin` tinyint(1) NOT NULL,
   `accomodates` int(5) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `workspace`
 --
 
 INSERT INTO `workspace` (`workspace_id`, `venue_id`, `user_id`, `type`, `space_name`, `similar_workspace`, `space_desc`, `workspace_pricing_id`, `image_1`, `image_2`, `image_3`, `image_4`, `image_5`, `image_6`, `image_7`, `image_8`, `image_9`, `image_10`, `listedbyuser`, `verifiedbyadmin`, `accomodates`) VALUES
-(1, 1, '1', 4, 'test', 3, 'test', 0, 'images/Creative-Office-Space1.jpg', '', '', '', '', '', '', '', '', '', 1, 0, 0),
-(2, 2, '2', 1, 'test', 3, 'test', 0, 'images/Creative-Office-Space1.jpg', '', '', '', '', '', '', '', '', '', 1, 0, 0),
-(3, 3, '2', 1, 'test', 3, 'test', 0, 'images/Creative-Office-Space1.jpg', '', '', '', '', '', '', '', '', '', 1, 0, 0);
+(1, 1, '2', 1, 'Flat For Lease', 1, 'Office goers can stay here', 0, 'images/Creative-Office-Space1.jpg', '', '', '', '', '', '', '', '', '', 1, 1, 0),
+(2, 2, '4', 1, 'Aravalli', 1, 'rent out for Houses', 0, 'images/Creative-Office-Space.jpg', 'images/Creative-Office-Space4.jpg', '', '', '', '', '', '', '', '', 1, 1, 0),
+(3, 3, '4', 1, 'Aravalli', 1, 'Office goers can rent this place', 0, 'images/Creative-Office-Space.jpg', '', '', '', '', '', '', '', '', '', 1, 1, 0);
 
 -- --------------------------------------------------------
 
@@ -335,16 +326,16 @@ CREATE TABLE IF NOT EXISTS `workspace_pricing` (
   `hourly_price` int(11) NOT NULL,
   `weekly_price` int(11) NOT NULL,
   `monthly_price` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `workspace_pricing`
 --
 
 INSERT INTO `workspace_pricing` (`workspace_pricing_id`, `workspace_id`, `venue_id`, `hourly_price`, `weekly_price`, `monthly_price`) VALUES
-(1, 1, 1, 80, 8, 18),
-(2, 0, 2, 20, 20, 50),
-(3, 0, 3, 20, 20, 50);
+(1, 1, 1, 500, 6000, 20000),
+(2, 2, 2, 500, 5000, 25000),
+(3, 3, 3, 200, 2000, 12000);
 
 --
 -- Indexes for dumped tables
@@ -404,6 +395,12 @@ ALTER TABLE `venue`
   ADD UNIQUE KEY `venue_id` (`venue_id`);
 
 --
+-- Indexes for table `verification`
+--
+ALTER TABLE `verification`
+  ADD PRIMARY KEY (`verification_id`);
+
+--
 -- Indexes for table `workspace`
 --
 ALTER TABLE `workspace`
@@ -423,12 +420,12 @@ ALTER TABLE `workspace_pricing`
 -- AUTO_INCREMENT for table `amenities`
 --
 ALTER TABLE `amenities`
-  MODIFY `amenity_id` int(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+  MODIFY `amenity_id` int(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `booking`
 --
 ALTER TABLE `booking`
-  MODIFY `booking_id` int(30) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=29;
+  MODIFY `booking_id` int(30) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `notification`
 --
@@ -438,7 +435,7 @@ ALTER TABLE `notification`
 -- AUTO_INCREMENT for table `reviews`
 --
 ALTER TABLE `reviews`
-  MODIFY `review_id` int(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+  MODIFY `review_id` int(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `spacetype`
 --
@@ -448,27 +445,32 @@ ALTER TABLE `spacetype`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `userid` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+  MODIFY `userid` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `user_details`
 --
 ALTER TABLE `user_details`
-  MODIFY `user_detail_id` int(50) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+  MODIFY `user_detail_id` int(50) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `venue`
 --
 ALTER TABLE `venue`
-  MODIFY `venue_id` int(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+  MODIFY `venue_id` int(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `verification`
+--
+ALTER TABLE `verification`
+  MODIFY `verification_id` int(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT for table `workspace`
 --
 ALTER TABLE `workspace`
-  MODIFY `workspace_id` int(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+  MODIFY `workspace_id` int(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `workspace_pricing`
 --
 ALTER TABLE `workspace_pricing`
-  MODIFY `workspace_pricing_id` int(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+  MODIFY `workspace_pricing_id` int(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
